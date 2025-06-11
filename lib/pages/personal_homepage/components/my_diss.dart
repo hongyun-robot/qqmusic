8aa26f028256e9cfd05885448ca9b4d1c9abe1b1
Hongyun <meng950406@163.com>
2025-06-11 15:48:07 +0800

    feat: 完成 创建的歌单

--- lib/pages/personal_homepage/components/my_diss.dart
+++ lib/pages/personal_homepage/components/my_diss.dart
@@ -6,9 +6,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qqmusic/bloc/user_bloc.dart';
+import 'package:qqmusic/components/z_icon/z_icon.dart' show ZIcon;
+import 'package:qqmusic/const/icon-style.dart' show ICON_STYLE;
 
-class MyDiss extends StatelessWidget {
+class MyDiss extends StatefulWidget {
   const MyDiss({super.key});
+
+  @override
+  State<MyDiss> createState() => _MyDissState();
+}
+
+class _MyDissState extends State<MyDiss> {
+  int? current;
+  int? currentItem;
+  int? currentItemTitle;
+  final Color maskColor = Color.fromRGBO(0, 0, 0, 0.5);
+  final Matrix4 translationValues = Matrix4.translationValues(0, -10, 0);
 
   @override
   Widget build(BuildContext context) {
@@ -51,29 +64,116 @@ class MyDiss extends StatelessWidget {
               children:
                   mydiss.mlist
                       .map(
-                        (i) => Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            MouseRegion(
-                              cursor: SystemMouseCursors.click,
-                              child: Container(
-                                clipBehavior: Clip.antiAlias,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Image.network(
-                                  i.picurl,
+                        (i) => MouseRegion(
+                          onEnter: (event) {
+                            setState(() {
+                              currentItem = i.dissid;
+                            });
+                          },
+                          onExit: (event) {
+                            setState(() {
+                              currentItem = null;
+                            });
+                          },
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              MouseRegion(
+                                cursor: SystemMouseCursors.click,
+                                onEnter: (event) {
+                                  setState(() {
+                                    current = i.dissid;
+                                  });
+                                },
+                                onExit: (event) {
+                                  setState(() {
+                                    current = null;
+                                  });
+                                },
+                                child: AnimatedContainer(
                                   width: itemWidth,
-                                  fit: BoxFit.fitWidth,
+                                  transform:
+                                      current == i.dissid
+                                          ? translationValues
+                                          : Matrix4.translationValues(0, 0, 0),
+                                  duration: Duration(milliseconds: 200),
+                                  clipBehavior: Clip.antiAlias,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Image.network(
+                                        i.picurl,
+                                        width: itemWidth,
+                                        fit: BoxFit.fitWidth,
+                                      ),
+                                      AnimatedContainer(
+                                        duration: Duration(milliseconds: 150),
+                                        color:
+                                            current == i.dissid
+                                                ? maskColor
+                                                : Colors.transparent,
+                                        height: itemWidth,
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(height: 10),
-                            MouseRegion(
-                              cursor: SystemMouseCursors.click,
-                              child: Text(i.title),
-                            ),
-                          ],
+                              SizedBox(height: 10),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                    flex: 10,
+                                    child: MouseRegion(
+                                      onEnter: (event) {
+                                        setState(() {
+                                          currentItemTitle = i.dissid;
+                                        });
+                                      },
+                                      onExit: (event) {
+                                        setState(() {
+                                          currentItemTitle = null;
+                                        });
+                                      },
+                                      cursor: SystemMouseCursors.click,
+                                      child: Text(
+                                        i.title,
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          color:
+                                              currentItemTitle == i.dissid
+                                                  ? ICON_STYLE.hoverColor
+                                                  : null,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Flexible(
+                                    flex: 1,
+                                    child:
+                                        currentItem == i.dissid
+                                            ? MouseRegion(
+                                              cursor: SystemMouseCursors.click,
+                                              child: ZIcon(
+                                                icon:
+                                                    Icons
+                                                        .delete_outline_rounded,
+                                                color: ICON_STYLE.defaultColor,
+                                                hoverColor:
+                                                    ICON_STYLE.hoverColor,
+                                                size: 16,
+                                              ),
+                                            )
+                                            : SizedBox(),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       )
                       .toList(),
